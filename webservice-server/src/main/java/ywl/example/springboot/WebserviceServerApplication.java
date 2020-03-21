715b7b08a622eb0ee3372696d9aa5fa6109901e1
yanweiling <yanweilingjob@126.com>
2020-03-21 16:04:20 +0800

    webservice

---
+++ webservice-server/src/main/java/ywl/example/springboot/WebserviceServerApplication.java
@@ -0,0 +1,13 @@
+package ywl.example.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebserviceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebserviceServerApplication.class, args);
+	}
+
+}
---
+++ webservice-server/src/test/java/ywl/example/springboot/WebserviceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ywl.example.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebserviceServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
